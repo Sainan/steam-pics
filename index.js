0bc77abb256ad5fa3681bf7e44df94343d9ec183
steam-pics <pics@steam>
2025-09-21 12:45:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const child_process = require("child_process");
+const fs = require("fs");
+const SteamUser = require("steam-user");
+
+const user = new SteamUser({
+	enablePicsCache: true,
+	picsCacheAll: true,
+	changelistUpdateInterval: 60_000,
+});
+user.logOn({ anonymous: true });
+user.on("loggedOn", () => {
+	console.log(`Logged on to Steam as ${user.steamID.steam3()}`);
+});
+user.on("appUpdate", (appId, data) => {
+	console.log(`Change ${data.changenumber}`);
+	fs.writeFileSync("appinfo/" + appId + ".json", JSON.stringify(data.appinfo, null, 2));
+	child_process.execSync(`git add appinfo/${appId}.json`);
+	child_process.execSync(`git commit -m "Change ${data.changenumber} (missingToken=${data.missingToken})"`)
+});
